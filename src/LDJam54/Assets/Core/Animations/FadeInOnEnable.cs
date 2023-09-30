67e46ceb518f89a90403430dc4284a97ef769f07
SilasReinagel <silas.reinagel@gmail.com>
2023-09-30 03:59:14 -0500

    Coding: On Fired, Shows Game Over. Closes #74

---
+++ src/LDJam54/Assets/Core/Animations/FadeInOnEnable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class FadeInOnEnable : MonoBehaviour
+{
+   [SerializeField] public float fadeInDuration; 
+   
+   private CanvasGroup _canvasGroup;
+   private float _elapsedTime = 0f;
+
+   private void OnEnable()
+   {
+      _canvasGroup = gameObject.GetComponent<CanvasGroup>();
+      _elapsedTime = 0f;
+   }
+
+   private void FixedUpdate()
+   {
+      _elapsedTime += Time.unscaledDeltaTime;
+      if (_canvasGroup.alpha < 1)
+      {
+         _canvasGroup.alpha = Mathf.Lerp(0, 1, _elapsedTime / fadeInDuration);
+      }
+      else
+      {
+         _canvasGroup.alpha = 1;
+         enabled = false;
+      }
+   }
+}
---
+++ src/LDJam54/Assets/Scripts/Conclusion/GameOverUiController.cs
@@ -0,0 +1,19 @@
+﻿using TextFx;
+using UnityEngine;
+
+public class GameOverUiController : OnMessage<WasFired>
+{
+    public GameObject target;
+    public TextFxTextMeshProUGUI text;
+    
+    protected override void AfterEnable()
+    {
+        target.SetActive(false);
+        text.AnimationManager.PlayAnimation();
+    }
+    
+    protected override void Execute(WasFired msg)
+    {
+        target.SetActive(true);
+    }
+}
