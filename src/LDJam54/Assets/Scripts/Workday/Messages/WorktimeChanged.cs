ce3321cb959557ba419b1470ff89e1395439a04c
SilasReinagel <silas.reinagel@gmail.com>
2023-09-30 02:34:13 -0500

    Coding: Workday Time System + Clock. Closes #42

---
+++ src/LDJam54/Assets/Scripts/Workday/Messages/WorktimeChanged.cs
@@ -0,0 +1,12 @@
+﻿
+public class WorktimeChanged
+{
+    public int CurrentHour { get; private set; }
+    public int CurrentMinute { get; private set; }
+
+    public WorktimeChanged(int currentHour, int currentMinute)
+    {
+        CurrentHour = currentHour;
+        CurrentMinute = currentMinute;
+    }
+}
---
+++ src/LDJam54/Assets/Scripts/Workday/WorkdayConfig.cs
@@ -0,0 +1,8 @@
+﻿
+public class WorkdayConfig
+{
+    public const int START_OF_DAY = 8;
+    public const int END_OF_DAY = 17;
+
+    public const float NUM_SECONDS_PER_WORK_MINUTE = 1f / 9f;
+}
