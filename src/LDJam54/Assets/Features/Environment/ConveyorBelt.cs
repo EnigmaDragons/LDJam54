7c4f34698fc8a8b00c03cbdbf18aa6ff13d6d8a4
Ivan Yakimov <legendthechaos@gmail.com>
2023-10-01 01:53:32 +0600

    Added ConveyorBelt.prefab

---
+++ src/LDJam54/Assets/Features/Environment/ConveyorBelt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using Util;
+
+namespace Features.Environment
+{
+    public class ConveyorBelt : MonoBehaviour
+    {
+        [SerializeField] 
+        private ColliderWrapper topCollider;
+        [SerializeField]
+        private float speed = 1.0f;
+        private void Awake()
+        {
+            topCollider.OnTriggerEnterAction += OnTriggerEnter;
+            topCollider.OnTriggerExitAction += OnTriggerExit;
+        }
+
+        private void OnTriggerEnter(Collider obj)
+        {
+            if(!obj.gameObject.HasComponent<Rigidbody>()) return;
+            obj.gameObject.AddComponent<ObjectOnConveyor>().velocity = transform.forward * -speed;
+        }
+
+        private void OnTriggerExit(Collider obj)
+        {
+            if(!obj.gameObject.HasComponent<Rigidbody>()) return;
+            Destroy(obj.gameObject.GetComponent<ObjectOnConveyor>());
+        }
+    }
+}
