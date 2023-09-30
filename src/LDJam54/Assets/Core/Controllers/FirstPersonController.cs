68763a9d4ddf73dc192ffb418a3828fdd0565054
SilasReinagel <silas.reinagel@gmail.com>
2023-09-30 18:13:46 -0500

    Revert FPController script

--- src/LDJam54/Assets/Core/Controllers/FirstPersonController.cs
+++ src/LDJam54/Assets/Core/Controllers/FirstPersonController.cs
@@ -1,59 +1,85 @@
-﻿using UnityEngine;
+﻿// FPS Controller
+// 1. Create a Parent Object like a 3D model
+// 2. Make the Camera the user is going to use as a child and move it to the height you wish. 
+// 3. Attach a Rigidbody to the parent
+// 4. Drag the Camera into the m_Camera public variable slot in the inspector
+// Escape Key: Escapes the mouse lock
+// Mouse click after pressing escape will lock the mouse again
+
+using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class FirstPersonController : MonoBehaviour
 {
     [SerializeField]
     private float speed = 5.0f;
-    [SerializeField]
-    private float m_lookSensitivity = 3.0f;
+    [SerializeField] private float m_lookSensitivity = 3.0f;
+    private float m_MovX;
+    private float m_MovY;
+    private Vector3 m_moveHorizontal;
+    private Vector3 m_movVertical;
     private Vector3 m_velocity;
     private Rigidbody m_Rigid;
+    private float m_yRot;
+    private float m_xRot;
+    private Vector3 m_rotation;
+    private Vector3 m_cameraRotation;
     private bool m_cursorIsLocked = true;
 
     [Header("The Camera the player looks through")]
     public Camera m_Camera;
 
+    
     public float Speed { get => speed; set => speed = value; }
-
+    // Use this for initialization
     private void Start()
     {
         m_Rigid = GetComponent<Rigidbody>();
         LockCursor();
     }
 
-    private void Update()
+    // Update is called once per frame
+    public void FixedUpdate()
     {
-        // Mouse movement 
-        float m_yRot = Input.GetAxisRaw("Mouse X") * m_lookSensitivity;
-        float m_xRot = Input.GetAxisRaw("Mouse Y") * m_lookSensitivity;
+        m_MovX = Input.GetAxis("Horizontal");
+        m_MovY = Input.GetAxis("Vertical");
 
-        // Apply camera rotation
-        if (m_Camera != null)
-        {
-            m_Camera.transform.Rotate(-m_xRot * Time.deltaTime, m_yRot * Time.deltaTime, 0);
-        }
-
-        InternalLockUpdate();
-    }
-
-    private void FixedUpdate()
-    {
-        float m_MovX = Input.GetAxis("Horizontal");
-        float m_MovY = Input.GetAxis("Vertical");
-
-        Vector3 m_moveHorizontal = transform.right * m_MovX;
-        Vector3 m_movVertical = transform.forward * m_MovY;
+        m_moveHorizontal = transform.right * m_MovX;
+        m_movVertical = transform.forward * m_MovY;
 
         m_velocity = (m_moveHorizontal + m_movVertical).normalized * speed;
 
-        // Move the actual player here
+        //mouse movement 
+        m_yRot = Input.GetAxisRaw("Mouse X");
+        m_rotation = new Vector3(0, m_yRot, 0) * m_lookSensitivity;
+
+        m_xRot = Input.GetAxisRaw("Mouse Y");
+        m_cameraRotation = new Vector3(m_xRot, 0, 0) * m_lookSensitivity;
+
+        //apply camera rotation
+
+        //move the actual player here
         if (m_velocity != Vector3.zero)
         {
             m_Rigid.MovePosition(m_Rigid.position + m_velocity * Time.fixedDeltaTime);
         }
-    }
 
+        if (m_rotation != Vector3.zero)
+        {
+            //rotate the camera of the player
+            m_Rigid.MoveRotation(m_Rigid.rotation * Quaternion.Euler(m_rotation));
+        }
+
+        if (m_Camera != null)
+        {
+            //negate this value so it rotates like a FPS not like a plane
+            m_Camera.transform.Rotate(-m_cameraRotation);
+        }
+
+        InternalLockUpdate();
+    }
+    
+    //controls the locking and unlocking of the mouse
     private void InternalLockUpdate()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
@@ -69,7 +95,7 @@ public class FirstPersonController : MonoBehaviour
         {
             UnlockCursor();
         }
-        else
+        else if (!m_cursorIsLocked)
         {
             LockCursor();
         }
