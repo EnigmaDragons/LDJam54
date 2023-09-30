8b526f1ea26b805370a888dc695dabdfced3ccc5
SilasReinagel <silas.reinagel@gmail.com>
2023-09-30 02:57:29 -0500

    Coding: Working on Stairwell

--- src/LDJam54/Assets/Scripts/Infrastructure/Navigator.cs
+++ src/LDJam54/Assets/Scripts/Infrastructure/Navigator.cs
@@ -3,16 +3,14 @@ using UnityEngine;
 [CreateAssetMenu]
 public sealed class Navigator : ScriptableObject
 {
-    [SerializeField] private bool loggingEnabled;
-    
-    public void NavigateToMainMenu() => NavigateTo("MainMenu");
-    public void NavigateToGameScene() => NavigateTo("GameScene");
-    public void NavigateToScene(string sceneName) => NavigateTo(sceneName);
+    public static void NavigateToMainMenu() => NavigateTo("MainMenu");
+    public static void NavigateToGameScene() => NavigateTo("GameScene");
+    public static void NavigateToCreditsScene() => NavigateTo("CreditsScene");
+    public static void NavigateToScene(string sceneName) => NavigateTo(sceneName);
 
-    private void NavigateTo(string sceneName)
+    private static void NavigateTo(string sceneName)
     {
-        if (loggingEnabled)
-            Log.Info($"Navigating to {sceneName}");
+        Log.Info($"Navigating to {sceneName}");
         Message.Publish(new NavigateToSceneRequested(sceneName));
     }
 }
