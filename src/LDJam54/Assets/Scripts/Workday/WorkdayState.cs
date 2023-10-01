45a0e97781adf162b30ce1317bc8751104971927
SilasReinagel <silas.reinagel@gmail.com>
2023-10-01 12:33:08 -0500

    Coding: Publishes Messages for Workday 70% over and Final Hour. Closes #193. Closes #195

--- src/LDJam54/Assets/Scripts/Workday/WorkdayState.cs
+++ src/LDJam54/Assets/Scripts/Workday/WorkdayState.cs
@@ -5,6 +5,7 @@ public static class WorkdayState
     public static int CurrentMinute;
     public static bool IsWorkdayStarted;
     public static bool IsWorkdayEnded;
+    public static bool PublishedFinalHourMessage;
 
     public static void InitWorkday()
     {
@@ -12,29 +13,9 @@ public static class WorkdayState
         CurrentMinute = 0;
         IsWorkdayStarted = false;
         IsWorkdayEnded = false;
+        PublishedFinalHourMessage = false;
     }
     
-    public static void AdvanceTime(int minutes)
-    {
-        CurrentMinute += minutes;
-        if (CurrentMinute >= 60)
-        {
-            CurrentHour++;
-            CurrentMinute -= 60;
-        }
-        if (CurrentHour >= 24)
-        {
-            CurrentHour -= 24;
-        }
-        Message.Publish(new WorktimeChanged(CurrentHour, CurrentMinute));
-
-        if (CurrentHour == WorkdayConfig.END_OF_DAY)
-        {
-            IsWorkdayEnded = true;
-            Message.Publish(new WorkdayEnded());
-        }
-    }
-
     public static void SetTotalWorkdayMinutes(int minutes)
     {
         CurrentHour = WorkdayConfig.START_OF_DAY;
@@ -49,6 +30,15 @@ public static class WorkdayState
             CurrentHour -= 24;
         }
         Message.Publish(new WorktimeChanged(CurrentHour, CurrentMinute));
+        var fractionOfDay = minutes / ((WorkdayConfig.END_OF_DAY - WorkdayConfig.START_OF_DAY) * 60f);
+        if (fractionOfDay > 0.7f)
+            Message.Publish(new WorkdayNearlyOver());
+        
+        if (CurrentHour + 1 == WorkdayConfig.END_OF_DAY && !PublishedFinalHourMessage)
+        {
+            PublishedFinalHourMessage = true;
+            Message.Publish(new WorkdayFinalHourStarted());
+        }
 
         if (CurrentHour == WorkdayConfig.END_OF_DAY)
         {
