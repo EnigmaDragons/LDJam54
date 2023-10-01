27191ff8037c5769b8ff05ba9512e93c82cede4b
SilasReinagel <silas.reinagel@gmail.com>
2023-10-01 16:21:24 -0500

    Morning Room Config. Closes #209

---
+++ src/LDJam54/Assets/Scripts/Workday/MeetingTimeController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MeetingTimeController : OnMessage<GameStateChanged>
+{
+    public GameObject eveningElement;
+    public GameObject morningElement;
+
+    private void Start() => Render(CurrentGameState.State.MeetingTime);
+    
+    protected override void Execute(GameStateChanged msg) => Render(msg.State.MeetingTime);
+
+    private void Render(MeetingTime mt)
+    {
+        eveningElement.SetActive(mt == MeetingTime.Evening);
+        morningElement.SetActive(mt == MeetingTime.Morning);
+    }
+}
