cb1e63c3eeca20c3677432c0ca493d5371aead83
Ivan Yakimov <legendthechaos@gmail.com>
2023-10-01 20:08:50 +0600

    Refine game objects interactions and properties
    Several changes were made to improve the game's mechanics and interactions. Firstly, unnecessary prefab instances were removed from the PlayerPlayground scene for better efficiency. Furthermore, BoxDelivered logic was extended in the BoxDelivered.cs script to include the box color, refining the way boxes are handled and delivered. In the ShippingArea.cs script, color validation was added to ensure only boxes with the correct color are processed. Lastly, the physics of the JumpPad was improved by setting the y-component of the player's velocity to zero before the jump force is applied for a more uniform jump behavior.

--- src/LDJam54/Assets/Features/Interaction/ShippingArea.cs
+++ src/LDJam54/Assets/Features/Interaction/ShippingArea.cs
@@ -7,18 +7,20 @@ namespace Features.Interaction
 {
     public class ShippingArea : MonoBehaviour
     {
+        [SerializeField] private SortingColor color;
         private void OnTriggerEnter(Collider other)
         {
-            if (other.gameObject.TryGetComponent(out InteractableObject interactable))
-            {
-                interactable.PlayShippedAnimation();
-                DeliverBox();
-            }
+            if(!other.gameObject.TryGetComponent(out InteractableObject interactable)) return;
+            if(!other.gameObject.TryGetComponent(out ColoredBox box)) return;
+            if (box.Color != color) return;
+            
+            interactable.PlayShippedAnimation();
+            DeliverBox();
         }
 
         private void DeliverBox()
         {
-            var message = new BoxDelivered();
+            var message = new BoxDelivered(color);
             Message.Publish(message);
             CurrentGameState.IncrementKPIStatic(KPI.BoxShipped, 1);
         }
