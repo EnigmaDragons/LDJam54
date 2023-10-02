5b0537a5ee331f39254ee42f6bac6cf9a8f431c2
SilasReinagel <silas.reinagel@gmail.com>
2023-10-02 02:51:57 -0500

    Boss Firing You Speech. Closes #279

---
+++ src/LDJam54/Assets/Scripts/PerformanceReview/OnPlayerEnterStartMeetingSpeech.cs
@@ -0,0 +1,19 @@
+﻿
+public class OnPlayerEnterStartMeetingSpeech : PlayerTrigger
+{
+    public GameConfig cfg;
+    
+    protected override void OnTriggered()
+    {
+        if (CurrentGameState.State.PlayerIsFired)
+            return;
+
+        var dayConfig = cfg.CurrentDayConfig;
+        var meetingTime = CurrentGameState.State.MeetingTime;
+
+        if (meetingTime == MeetingTime.Evening && !dayConfig.AfternoonSpeech.IsNull)
+        {
+            Message.Publish(new PlayMeetingBossMonologue(dayConfig.AfternoonSpeech));
+        }
+    }
+}
