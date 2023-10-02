7f2ab990a30c10c94c50c5b35a4cc73393d2edba
Tim Reinagel <tmr2_handbell@yahoo.com>
2023-10-02 16:45:54 -0700

    Prepull Commit

--- src/LDJam54/Assets/Scripts/PerformanceReview/MeetingSpeaker.cs
+++ src/LDJam54/Assets/Scripts/PerformanceReview/MeetingSpeaker.cs
@@ -1,13 +1,22 @@
-﻿using FMODUnity;
+﻿using FMOD.Studio;
+using FMODUnity;
 
 public class MeetingSpeaker : OnMessage<PlayMeetingBossMonologue>
 {
     public StudioEventEmitter emitter;
-    
+
+    private EventInstance instance;
+
     protected override void Execute(PlayMeetingBossMonologue msg)
     {
-        emitter.Stop();
-        emitter.EventReference = msg.Speech;
-        emitter.Play();
+        instance = emitter.EventInstance;
+        if (instance.isValid())
+        {
+            instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            instance.release();
+        }
+        instance = RuntimeManager.CreateInstance(msg.Speech);
+        instance.start();
+        instance.release();
     }
 }
