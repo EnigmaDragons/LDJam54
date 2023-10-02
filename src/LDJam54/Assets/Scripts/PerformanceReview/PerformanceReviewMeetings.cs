e86d23d9e6cc176303f3738d48d33c4e61b8260d
SilasReinagel <silas.reinagel@gmail.com>
2023-10-01 19:30:49 -0500

    On Workday Ended, Triggers Performance Review. Closes #239

--- src/LDJam54/Assets/Scripts/PerformanceReview/PerformanceReviewMeetings.cs
+++ src/LDJam54/Assets/Scripts/PerformanceReview/PerformanceReviewMeetings.cs
@@ -3,6 +3,8 @@ public class PerformanceReviewMeetings : OnMessage<WorkdayEnded>
 {
     protected override void Execute(WorkdayEnded msg)
     {
+        PerformanceEvaluator.Evaluate();
+        CurrentGameState.Update(g => g.MeetingTime = MeetingTime.Evening);
         Message.Publish(new StartPerformanceMeetingRequested());
     }
 }
