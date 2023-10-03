a1c80990edb805bb463417a0887bf6b503dc68af
SilasReinagel <silas.reinagel@gmail.com>
2023-10-03 13:09:33 -0500

    UI: Added In-Game Menu with Exit Game button. Closes #233

---
+++ src/LDJam54/Assets/Core/Inputs/KeepMouseCursorVisibleWhileEnabled.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class KeepMouseCursorVisibleWhileEnabled : MonoBehaviour
+{
+    private void OnEnable() => MouseState.AddCursorVisibleLock();
+    private void OnDisable() => MouseState.RemoveCursorVisibleLock();
+}
---
+++ src/LDJam54/Assets/Core/Inputs/MouseState.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public static class MouseState
+{
+    private static int _numCursorVisibleLocks = 0;
+
+    public static void AddCursorVisibleLock()
+    {
+        _numCursorVisibleLocks++;
+        ShowMouseCursor();
+    }
+
+    public static void RemoveCursorVisibleLock()
+    {
+        _numCursorVisibleLocks--;
+        HideMouseCursor();
+    }
+    
+    public static void HideMouseCursor() => SetCursorVisible(false);
+    public static void ShowMouseCursor() => SetCursorVisible(true);
+
+    public static void SetCursorVisible(bool shouldBeVisible)
+    {
+        var finalShouldBeVisible = shouldBeVisible || _numCursorVisibleLocks > 0;
+        
+        Cursor.lockState = !finalShouldBeVisible ? CursorLockMode.Locked : CursorLockMode.None;
+        Cursor.visible = finalShouldBeVisible;
+        Debug.Log($"Cursor should be shown: {finalShouldBeVisible}. LockState: {Cursor.lockState}. Visible: {Cursor.visible}");
+    }
+}
